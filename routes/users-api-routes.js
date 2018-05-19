d2305ef795e68e7092d8e8c9cd75c06746dfcb48
mrudesyle@outlook.com <mrudesyle@outlook.com>
2018-05-19 13:52:02 -0600

    Updated routes

--- routes/users-api-routes.js
+++ routes/users-api-routes.js
@@ -11,5 +11,11 @@ var db = require("../models");
 // Routes
 // =============================================================
 module.exports = function(app) {
-
+    //get all users
+    app.get("/api/users", function(req, res) {
+        db.User.findAll({
+        }).then(function(dbUser) {
+          res.json(dbUser);
+        });
+      });
 };
