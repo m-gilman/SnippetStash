777cc28b26e749d900fc9140940a225f12483fe6
Melissa Gilman <melissadgilman@yahoo.com>
2018-05-21 15:17:04 -0600

    added path to index page after button clicks

--- public/js/home.js
+++ public/js/home.js
@@ -1,2 +1,8 @@
-console.log('working');
+$(document).ready(function () {
 
+    $("#login").click(function (event) {
+        event.preventDefault();
+        alert('We are working on a Login feature. Please click "Enter as a guest" to view our many snippets.');
+    });
+
+});
