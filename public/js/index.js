d65d6474c1573e2b455e995cac48cf2ca0548b95
Melissa Gilman <melissadgilman@yahoo.com>
2018-05-22 09:41:16 -0600

    placement of divs on index.html

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,11 @@
 $(document).ready(function () {
 
-
-
-    
+    // Modal with codeMirror
     $('#createSnippet').on('shown.bs.modal', function () {
         var code = $("#snippetCode")[0];
         var editor = CodeMirror.fromTextArea(code, {
             lineNumbers: true
         })
-
     });
 
     
