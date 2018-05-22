6a13b7507b51c51b4f8c627db732f53470b255d1
Melissa Gilman <melissadgilman@yahoo.com>
2018-05-22 10:13:44 -0600

    added theme to codemirror

--- public/js/index.js
+++ public/js/index.js
@@ -4,9 +4,10 @@ $(document).ready(function () {
     $('#createSnippet').on('shown.bs.modal', function () {
         var code = $("#snippetCode")[0];
         var editor = CodeMirror.fromTextArea(code, {
-            lineNumbers: true
+            lineNumbers: true,
+            theme: "midnight"
         })
     });
 
-    
+
 });
