212cdb5060ec82b759cd9c14b080a38f45823406
Melissa Gilman <melissadgilman@yahoo.com>
2018-05-22 08:20:01 -0600

    added code mirror to snippet modal

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,15 @@
-$('#createSnippet').on('shown.bs.modal', function () {
-    // $('#myInput').focus()
-  })
+$(document).ready(function () {
+
+
+
+    
+    $('#createSnippet').on('shown.bs.modal', function () {
+        var code = $("#snippetCode")[0];
+        var editor = CodeMirror.fromTextArea(code, {
+            lineNumbers: true
+        })
+
+    });
+
+    
+});
