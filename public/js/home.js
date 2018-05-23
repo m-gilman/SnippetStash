692be1b5db27d8ff3a7b94b3de60629c019f6e0e
Melissa Gilman <melissadgilman@yahoo.com>
2018-05-22 21:11:43 -0600

    added New User Modal

--- public/js/home.js
+++ public/js/home.js
@@ -5,4 +5,8 @@ $(document).ready(function () {
         alert('We are working on a Login feature. Please click "Enter as a guest" to view our many snippets.');
     });
 
+    // New User Sign Up Modal Trigger
+    $('#newUser').on('shown.bs.modal', function () {
+    });
+
 });
