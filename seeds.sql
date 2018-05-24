2abd27398e9a1a9c86c113e9c7129c8e0c555861
mrudesyle <mrudesyle@outlook.com>
2018-05-24 13:16:03 -0600

    Update seeds.sql

--- seeds.sql
+++ seeds.sql
@@ -1,10 +1,29 @@
+/*
+
+DROP DATABASE IF EXISTS snippetStash;
+CREATE DATABASE snippetStash;
+*/
+
 USE snippetStash;
 INSERT INTO users(username, password, createdAt, updatedAt) VALUES(
 'Admin','ABC123!',  NOW(),  NOW());
+SELECT * FROM users;
 
 INSERT INTO categories (catName, createdAt, updatedAt) VALUES
-('Sequelize', NOW(), NOW()),
-('Handelebars', NOW(), NOW()),
-('Testing', NOW(), NOW()),
+('Ajax', NOW(), NOW()),
+('CSS', NOW(), NOW()),
 ('Express', NOW(), NOW()), 
-('HTML',NOW(),NOW());
+('FireBase',NOW(),NOW()),
+('Handelebars', NOW(), NOW()),
+('HTML',NOW(),NOW()),
+('JavaScript', NOW(), NOW()),
+('JQuery', NOW(), NOW()),
+('JS Constructors', NOW(), NOW()),
+('MySql',NOW(),NOW()),
+('Node.js',NOW(),NOW()),
+('Sequelize', NOW(), NOW()),
+('Testing', NOW(), NOW()),
+('Timers',NOW(),NOW());
+
+SELECT *
+FROM categories;
