f66c5aaa67646049f39e4e45b991a9822331ba2c
mrudesyle@outlook.com <mrudesyle@outlook.com>
2018-05-28 00:37:31 -0600

    Another update - fixed some bugs

--- seeds.sql
+++ seeds.sql
@@ -5,15 +5,10 @@ CREATE DATABASE snippetStash;
 */
 
 USE snippetStash;
-<<<<<<< HEAD
 
 INSERT INTO users (id, email, password, createdAt, updatedAt) VALUES
 (default, 'admin@admin.com', '12345', NOW(), NOW());
 
-=======
-INSERT INTO users(username, password, createdAt, updatedAt) VALUES(
-'Admin','ABC123!',  NOW(),  NOW());
->>>>>>> mark
 SELECT * FROM users;
 
 INSERT INTO categories (catName, createdAt, updatedAt) VALUES
@@ -25,20 +20,15 @@ INSERT INTO categories (catName, createdAt, updatedAt) VALUES
 ('HTML',NOW(),NOW()),
 ('JavaScript', NOW(), NOW()),
 ('JQuery', NOW(), NOW()),
-('JS Constructors', NOW(), NOW()),
+('JSConstructors', NOW(), NOW()),
 ('MySql',NOW(),NOW()),
 ('Node.js',NOW(),NOW()),
 ('Sequelize', NOW(), NOW()),
 ('Testing', NOW(), NOW()),
 ('Timers',NOW(),NOW());
 
-<<<<<<< HEAD
 SELECT * FROM categories;
-=======
+
 SELECT *
-<<<<<<< HEAD
 FROM categories;
-=======
-FROM categories;
->>>>>>> 2abd27398e9a1a9c86c113e9c7129c8e0c555861
->>>>>>> mark
+
