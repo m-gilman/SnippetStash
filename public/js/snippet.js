a119ee544531b6b85d435785ff898e0c0473eb0e
mrudesyle@outlook.com <mrudesyle@outlook.com>
2018-05-31 22:42:33 -0600

    Fixed routing & navbar links to add public and member views

--- public/js/snippet.js
+++ public/js/snippet.js
@@ -1,17 +1,8 @@
-$(document).ready(function () {
 
+/////////////////////////////////////////////////////////////code for filtering by categoryID & userID
+$(document).ready(function () {
     var url = window.location.search;
     var dir = window.location.pathname;
-    if (url.indexOf("?catId=") !== -1) {
-        catId = url.split("=")[1];    
-        getAllSnippetsByCategory(catId);
-      }
-    else{
-        //displays first category snippets as default view
-        catId = 1;
-        getAllSnippetsByCategory(catId);
-    }
-        
     var stashForm = $("#stash-form");
     var titleInput = $("#SnippetTitle");
     var contentInput = $("#snippetCode");
@@ -19,44 +10,42 @@ $(document).ready(function () {
     var usrName = $(".member-id");
     var catId = $("#snippetCategory");
     var header = $("#snippetHdr");
-    
-    
+    var memberId;
 
-
-    $(stashForm).on("submit", function handleFormSubmit(event) {
-        event.preventDefault();
-        // Wont submit the post if we are missing title, content, or username
-        if (!titleInput.val().trim() || !contentInput.val().trim() || !usrName.text()) {
-            console.log("Form Fields need to be completed!!!")
-            console.log("Title Input: " + titleInput.val().trim());
-            console.log("Snippet Code: " + contentInput.val().trim());
-            console.log("Member Email: " + usrName.text());
-            return;
-        }
-        // Constructing a newPost object to hand to the database
-        var newSnippet = {
-            snippetTitle: titleInput.val().trim(),
-            snippetContent: contentInput.val().trim(),
-            snippetDescription: descriptionInput.val().trim(),
-            CategoryId: catId.val(),
-            UserId: usrName.text()
-        };
-
-        insertSnippet(newSnippet);
-
+    //get user data & set welcome header
+    $.get("/api/user_data").then(function (data) {
+        $(".member-name").text(data.email);
+        $(".member-id").text(data.id);
+        var uid = data.id;
+        getMember(uid);
     });
 
-    function insertSnippet(newSnippet) {
-        // console.log("Title Input: " + titleInput.val().trim());
-        // console.log("Snippet Code: " + contentInput.val().trim());
-        // console.log("Member Email: " + usrName.text());
-        $.post("/api/snippets", newSnippet, function () {
-            window.location.href = dir +"?catId=" + catId.val();
-        });
-    };
+    //pass member id it use it to return snippets
+    function getMember(uid) {
+        memberId = uid;
+        if (url.indexOf("?catId=") !== -1) {
+            catId = url.split("=")[1];    
+            if(dir === "/public"){
+                getAllSnippetsByCategory(catId);
+            }else{
+                getMemberSnippetsByCategory(catId); 
+            }
+                    
+          }
+        else{
+            //displays first category snippets as default view
+            catId = 1;
+            if(dir === "/public"){
+                getAllSnippetsByCategory(catId);
+            }else{
+                getMemberSnippetsByCategory(catId); 
+            }
+        }    
+
+    }
 
     function getAllSnippetsByCategory(id) {
-        $.get("/api/snippets/" +id, function (data) {
+        $.get("/api/snippets/" + id, function (data) {
             var itemToAdd = [];
             for (var i = 0; i < data.length; i++) {
                 itemToAdd.push(renderSnippets(data[i]));
@@ -65,21 +54,152 @@ $(document).ready(function () {
         });
     }
 
-    function getCatName(id){
-        $.get("/api/categories/" +id, function (data) {
-            console.log(data.catName);
+    function getMemberSnippetsByCategory(id) {
+        $.get("/api/user/snippets/" + id, function (data) {
+            var itemToAdd = [];
+            for (var i = 0; i < data.length; i++) {
+                itemToAdd.push(renderSnippets(data[i]));
+                console.log(data[i]);
+            }
+            getCatName(id);
+        });
+    }
+
+    function getCatName(id) {
+        $.get("/api/categories/" + id, function (data) {
             header.append(data.catName + " Snippets");
         });
     }
 
     function renderSnippets(itemToAdd) {
-        var newPanelHeading = $('<div class="panel ">').appendTo('#append-to-me');
-        newPanelHeading.append("<div class='col-lg-11 col-lg-12'> <div class='panel-group' id='accordion" + itemToAdd.id + "'> <div class='panel '> <div class='panel panel-primary'> <div class='panel-heading'> <div class='row'> <div class='col-xs-3'> <a href='' type='small-link' aria-label='Left Align'> <span class='glyphicon glyphicon-trash' id='deleteSnippet"+itemToAdd.id+" 'aria-hidden='true'></span> </a> </div><div class='col-xs-9 text-right'> <div class='huge'>" + itemToAdd.snippetTitle + "</div><div>" + itemToAdd.snippetDescription + "</div></div></div></div><a href='#'> <div class='snippetDetails'> <div class='panel-group' id='accordion" + itemToAdd.id + "'> <div class='panel panel-info'> <div class='panel-heading'> <h4 class='panel-title'> <a data-toggle='collapse' data-parent='#accordion" + itemToAdd.id + "' href='#collapse" + itemToAdd.id + "' > <span class='glyphicon glyphicon-plus'></span> View Details</a></h4></div><div id='collapse" + itemToAdd.id + "' class='panel-collapse collapse'> <div class='panel-body'> <div class='well'> <textarea class='form-control' rows='11'>" + itemToAdd.snippetContent + "</textarea> </div></div></div></div><div class='panel panel-info'> <div class='panel-heading'> <h4 class='panel-title'> <a data-toggle='collapse' data-parent='#accordion" + itemToAdd.id + "' href='#collapseTwo" + itemToAdd.id + "'> <span class='glyphicon glyphicon-plus'></span> Comments</a></h4></div><div id='collapseTwo" + itemToAdd.id + "' class='panel-collapse collapse'> <div class='panel-body'> <a href='#' class='btn btn-success' data-toggle='modal' data-target='#commentModal'>Add a Comment</a> <div> <br></div><div class='well'> <p>Comments go here.</p></div></div></div></div></div></div></a> </div></div></div></div>")
+        var newPanelHeading = $('<div class="panel panel-default">').appendTo('#append-to-me');
+        newPanelHeading.append("<div class='col-lg-11 col-lg-12' ><div class='panel-group' id='accordion" + itemToAdd.id + "'><div class='panel panel-default'><div class='panel panel-primary'><div class='panel-heading'><div class='row'><div class='col-xs-3'> <a href='' type='small-link' aria-label='Left Align'> <span class='glyphicon glyphicon-trash' id='deleteSnippet' aria-hidden='true'></span> </a></div><div class='col-xs-9 text-right'><div class='huge'>" + itemToAdd.snippetTitle + "</div><div>" + itemToAdd.snippetDescription + "</div></div></div></div> <a href='#'><div class='snippetDetails' ><div class='panel-group' id='accordion" + itemToAdd.id + "'><div class='panel panel-info'><div class='panel-heading'><h4 class='panel-title'> <a data-toggle='collapse' data-parent='#accordion" + itemToAdd.id + "' href='#collapse" + itemToAdd.id + "' > <span class='glyphicon glyphicon-plus'></span> View Details</a></h4></div><div id='collapse" + itemToAdd.id + "' class='panel-collapse collapse'><div class='panel-body'><div class='well'><textarea class='form-control' rows='11'>" + itemToAdd.snippetContent + "</textarea></div></div></div></div><div class='panel panel-info'><div class='panel-heading'><h4 class='panel-title'> <a data-toggle='collapse' data-parent='#accordion" + itemToAdd.id + "' href='#collapseTwo" + itemToAdd.id + "'> <span class='glyphicon glyphicon-plus'></span> Comments</a></h4></div><div id='collapseTwo" + itemToAdd.id + "' class='panel-collapse collapse'><div class='panel-body'> <a href='#' class='btn btn-success' data-toggle='modal' data-target='#commentModal'>Add a Comment</a><div> <br></div><div class='well'><p>Comments go here.</p></div></div></div></div></div></div> </a></div></div></div></div>")
     }
 
+        //form submission
+        $(stashForm).on("submit", function handleFormSubmit(event) {
+            event.preventDefault();
+            var catId = $("#snippetCategory");
+            // Wont submit the post if we are missing title, content, or username
+            if (!titleInput.val().trim() || !contentInput.val().trim() || !usrName.text()) {
+                console.log("Form Fields need to be completed!!!")
+                console.log("Title Input: " + titleInput.val().trim());
+                console.log("Snippet Code: " + contentInput.val().trim());
+                console.log("Member Email: " + usrName.text());
+                return;
+            }
+            // Constructing a newPost object to hand to the database
+            var newSnippet = {
+                snippetTitle: titleInput.val().trim(),
+                snippetContent: contentInput.val().trim(),
+                snippetDescription: descriptionInput.val().trim(),
+                CategoryId: catId.val(),
+                UserId: usrName.text()
+            };
+            insertSnippet(newSnippet);
     
-
+        });
     
-
+        function insertSnippet(newSnippet) {
+            var cid = newSnippet.CategoryId;
+            var mid = newSnippet.UserId;
+            // console.log("Title Input: " + titleInput.val().trim());
+            // console.log("Snippet Code: " + contentInput.val().trim());
+            // console.log("Member ID: " + mid);
+            // console.log("Category ID: " + cid );
+            $.post("/api/snippets", newSnippet, function () {
+                window.location.href = dir + "?catId=" + cid + "&userId=" + mid;
+            });
+        };
+        
 });//end document.ready function
- 
+
+
+
+
+
+
+// ///working code
+// $(document).ready(function () {
+
+//     var url = window.location.search;
+//     var dir = window.location.pathname;
+//     if (url.indexOf("?catId=") !== -1) {
+//         catId = url.split("=")[1];    
+//         getAllSnippetsByCategory(catId);
+//         console.log("IF Block: " + url);
+//       }
+//     else{
+//         //displays first category snippets as default view
+//         catId = 1;
+//         getAllSnippetsByCategory(catId);
+//         console.log("Else Block: " + url);
+//     }
+
+//     var stashForm = $("#stash-form");
+//     var titleInput = $("#SnippetTitle");
+//     var contentInput = $("#snippetCode");
+//     var descriptionInput = $("#snippetDescription");
+//     var usrName = $(".member-id");
+//     var catId = $("#snippetCategory");
+//     var header = $("#snippetHdr");
+
+
+//     $(stashForm).on("submit", function handleFormSubmit(event) {
+//         event.preventDefault();
+//         // Wont submit the post if we are missing title, content, or username
+//         if (!titleInput.val().trim() || !contentInput.val().trim() || !usrName.text()) {
+//             console.log("Form Fields need to be completed!!!")
+//             console.log("Title Input: " + titleInput.val().trim());
+//             console.log("Snippet Code: " + contentInput.val().trim());
+//             console.log("Member Email: " + usrName.text());
+//             return;
+//         }
+//         // Constructing a newPost object to hand to the database
+//         var newSnippet = {
+//             snippetTitle: titleInput.val().trim(),
+//             snippetContent: contentInput.val().trim(),
+//             snippetDescription: descriptionInput.val().trim(),
+//             CategoryId: catId.val(),
+//             UserId: usrName.text()
+//         };
+
+//         insertSnippet(newSnippet);
+
+//     });
+
+//     function insertSnippet(newSnippet) {
+//         // console.log("Title Input: " + titleInput.val().trim());
+//         // console.log("Snippet Code: " + contentInput.val().trim());
+//         // console.log("Member Email: " + usrName.text());
+//         $.post("/api/snippets", newSnippet, function () {
+//             window.location.href = dir +"?catId=" + catId.val();
+//         });
+//     };
+
+//     function getAllSnippetsByCategory(id) {
+//         $.get("/api/snippets/" +id, function (data) {
+//             var itemToAdd = [];
+//             for (var i = 0; i < data.length; i++) {
+//                 itemToAdd.push(renderSnippets(data[i]));
+//             }
+//             getCatName(id);
+//         });
+//     }
+
+//     function getCatName(id){
+//         $.get("/api/categories/" +id, function (data) {
+//             console.log(data.catName);
+//             header.append(data.catName + " Snippets");
+//         });
+//     }
+
+//     function renderSnippets(itemToAdd) {
+//         var newPanelHeading = $('<div class="panel ">').appendTo('#append-to-me');
+//         newPanelHeading.append("<div class='col-lg-11 col-lg-12' ><div class='panel-group' id='accordion" + itemToAdd.id + "'><div class='panel '><div class='panel panel-primary'><div class='panel-heading'><div class='row'><div class='col-xs-3'> <a href='' type='small-link' aria-label='Left Align'> <span class='glyphicon glyphicon-trash' id='deleteSnippet' aria-hidden='true'></span> </a></div><div class='col-xs-9 text-right'><div class='huge'>" + itemToAdd.snippetTitle + "</div><div>" + itemToAdd.snippetDescription + "</div></div></div></div> <a href='#'><div class='snippetDetails' ><div class='panel-group' id='accordion" + itemToAdd.id + "'><div class='panel panel-info'><div class='panel-heading'><h4 class='panel-title'> <a data-toggle='collapse' data-parent='#accordion" + itemToAdd.id + "' href='#collapse" + itemToAdd.id + "' > <span class='glyphicon glyphicon-plus'></span> View Details</a></h4></div><div id='collapse" + itemToAdd.id + "' class='panel-collapse collapse'><div class='panel-body'><div class='well'><textarea class='form-control' rows='11'>" + itemToAdd.snippetContent + "</textarea></div></div></div></div><div class='panel panel-info'><div class='panel-heading'><h4 class='panel-title'> <a data-toggle='collapse' data-parent='#accordion" + itemToAdd.id + "' href='#collapseTwo" + itemToAdd.id + "'> <span class='glyphicon glyphicon-plus'></span> Comments</a></h4></div><div id='collapseTwo" + itemToAdd.id + "' class='panel-collapse collapse'><div class='panel-body'> <a href='#' class='btn btn-success' data-toggle='modal' data-target='#commentModal'>Add a Comment</a><div> <br></div><div class='well'><p>Comments go here.</p></div></div></div></div></div></div> </a></div></div></div></div>")
+//     }
+
+// });//end document.ready function
+
+
+
--- routes/snippets-api-routes.js
+++ routes/snippets-api-routes.js
@@ -1,6 +1,6 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
 
   //create new snippet
   app.post("/api/snippets", function (req, res) {
@@ -17,18 +17,58 @@ module.exports = function(app) {
     });
   });
 
-   // Get route for returning snippets of a specific category
-   app.get("/api/snippets/:CategoryId", function(req, res) {
+  // Get route for returning snippets of a specific category
+  app.get("/api/snippets/:CategoryId", function (req, res) {
     // Add sequelize code to find all Snippets where the category is equal to req.params.category,
     // return the result to the user with res.json
-    db.Snippet.findAll({where: {
-      CategoryId: req.params.CategoryId
-    }}).then(function (dbSnippet){
+    db.Snippet.findAll({
+      where: {
+        CategoryId: req.params.CategoryId
+      }
+    }).then(function (dbSnippet) {
       res.json(dbSnippet);
     });
 
   });
-  
+
+  // app.get("/api/snippets/:CategoryId/:Uid", function(req, res) {
+  //   // Add sequelize code to find all Snippets where the category is equal to req.params.category,
+  //   // return the result to the user with res.json
+  //   db.Snippet.findAll({where: {
+  //     CategoryId: req.params.CategoryId,
+  //     UserId:  req.params.Uid
+  //   }}).then(function (dbSnippet){
+  //     res.json(dbSnippet);
+  //   });
+
+  //should update to this:
+  app.get("/api/user/snippets/:CategoryId/", function (req, res) {
+
+    // Add sequelize code to find all Snippets where the category is equal to req.params.category,
+    // return the result to the user with res.json
+    db.Snippet.findAll({
+      where: {
+        CategoryId: req.params.CategoryId,
+        UserId: req.user.id
+      }
+    }).then(function (dbSnippet) {
+      res.json(dbSnippet);
+    });
+  });
+
+  app.get("/api/user/", function (req, res) {
+
+    // Add sequelize code to find all Snippets where the category is equal to req.params.category,
+    // return the result to the user with res.json
+    db.Snippet.findOne({
+      where: {
+        UserId: req.user.id
+      }
+    }).then(function (dbSnippet) {
+      res.json(dbSnippet);
+    });
+  });
+
 
 };
 
